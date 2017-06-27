f8008d08cbcd0907024a2ce46a62570260b4e023
mak <makindosx@gmail.com>
2017-06-27 04:33:35 +0300

    Create index.php

---
+++ makbox/error/index.php
@@ -0,0 +1,65 @@
+<?php
+
+/*
+ * Copyright (c) 2016-2017 Barchampas Gerasimos <http://makbox.co.nf/>
+ * Makbox is a personal (staas) cloud.
+ *
+ * Makbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * Makbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+?>
+
+
+<html>
+<head>
+
+
+<style>
+ #a 
+ {
+ color:black;
+ text-decoration: none;
+  background-color:none;
+  }
+ </style>
+
+
+<link rel="stylesheet" type="text/css" href="error.css">
+
+</head>
+
+<body bgcolor="lightyellow">
+
+ <div align="left"> <img src="error1.png" height="150" width="170"> </div>
+ 
+  <br>
+
+
+ <div id="error">
+<h2> <font size="6"> Error 404: Not found  :-( </font> </h2>
+</div>
+
+ 
+  <br>
+
+  <div align="right"> <img src="error2.png" height="150" width="170"> </div>
+
+
+</body>
+</html>
+
+
