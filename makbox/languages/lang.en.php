b240a07a394436fbef04b4e79f3e8b7f839d2ca7
mak <makindosx@gmail.com>
2017-06-27 04:37:44 +0300

    Add files via upload

---
+++ makbox/languages/lang.en.php
@@ -0,0 +1,51 @@
+<?php
+
+/*
+ * Copyright (c) 2016-2017 Barchampas Gerasimos <http://makbox.co.nf/>
+ * Makbox is a personal (staas) cloud.
+ *
+ * Makbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * Makbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+
+
+/* 
+------------------
+Language: English
+------------------
+*/
+$lang = array();
+$lang['TITLE'] = 'Instructions for creating modules';
+$lang['RULE_1'] = '1) Create a subfolder in the folder modules';
+$lang['RULE_2'] = '2) Give one name to subfolder (app_name)';
+$lang['RULE_3'] = '3) Gopy file class_cn.php and compile.php from the (core) folder';
+$lang['RULE_4'] = '4) Greate your files for your app and insert into  <br>
+                      index.php file api_key and secret_key ';
+$lang['RULE_5'] = '5) Go to file user notice.php and insert <br>
+                      into head css name of your app';
+$lang['RULE_6'] = '6) Make a hyperlink which will take you to your page and give <br> 
+                      id the link that you set in css of your application';
+$lang['RULE_7'] = '7) Open file compile.php and give icon path <br> 
+                      and subfolder name of your module';
+$lang['RULE_8'] = '8) Run the compiled file to see if it works your module';
+$lang['RULE_9'] = '9) If your module operates activate it from here';
+$lang['RULE_10'] = '10) Delete files class_core.php and compile.php from your subfolder';
+$lang['NOTICE'] = '<b> <u> NOTICE: </u> </b> <br>
+                       In the folder modules there is an example <br>
+                       of how to create your application';
+$lang['MESSAGE'] = '<b> Create your module now! </b>';
+?>
---
+++ makbox/languages/lang.gr.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+ * Copyright (c) 2016-2017 Barchampas Gerasimos <http://makbox.co.nf/>
+ * Makbox is a personal (staas) cloud.
+ *
+ * Makbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * Makbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+
+/* 
+------------------
+Language: Greek
+------------------
+*/
+$lang = array();
+$lang['TITLE'] = 'Οδηγίες για τη δημιουργία ενοτήτων';
+$lang['RULE_1'] = '1) Δημιουργήστε έναν υποφάκελο μέσα στον φάκελο modules';
+$lang['RULE_2'] = '2) Δώστε ένα όνομα στον υποφάκελο (όνομά_εφαρμογής)';
+$lang['RULE_3'] = '3) Αντιγράψτε στον υποφάκελο σας τα αρχεία <br>
+                      class_cn.php και compile.php από τον φάκελο core';
+$lang['RULE_4'] = '4) Δημιουργήστε τα αρχεία σας και το ένθετο σας για την εφαρμογή σας και 
+                      εισάγετε στο αρχείο index.php το api_key και το secret_key σας';
+$lang['RULE_5'] = '5) Πηγαίνετε στο αρχείο user_notice.php και τοποθετήστε <br>
+                      στο header το όνομά tou css της εφαρμογής σας';
+$lang['RULE_6'] = '6) Δημιουργήστε ένα υπερσύνδεσμο που θα σας μεταφέρει <br>
+                      στη σελίδα σας και δώστε ως id στον υπερσύνδεσμο <br> 
+                      το id που έχετε ορίσει στο css της ενότητας σας ';
+$lang['RULE_7'] = '7) Ανοίξτε το αρχείο compile.php και δώστε διαδρομή <br> 
+                      για το εικονίδιο της ενότητας σας και ως όνομα  <br>
+                      το όνομα του υποφακέλου της μονάδας σας';
+$lang['RULE_8'] = '8) Εκτελέστε το μεταγλωττισμένο αρχείο (compile.php) <br> 
+                      για να δείτε αν λειτουργεί η ενότητα σας';
+$lang['RULE_9'] = '9) Αν η ενότητα σας λειτουργεί ενεργοποιήσετε την από εδώ';
+$lang['RULE_10'] = '10) Σβήστε τα αρχεία class_cn.php και compile.php από τον υποφάκελο σας';
+$lang['NOTICE'] = '<b> <u> ΕΙΔΟΠΟΙΗΣΗ: </u> </b> <br>
+                   <b> Στον φάκελο ενότητεςυπάρχει ένα παράδειγμα <br> 
+                       για το πώς να δημιουργήσετε την ενότητα  σας </b>';
+$lang['MESSAGE'] = '<b> Δημιουργήστε την ενότητα σας τώρα! </b>';
+?>
