1705e2fde0a7466cb81e3304ed71fadb75e0c9e2
mak <makindosx@gmail.com>
2017-06-27 05:36:30 +0300

    Create all_users_details.sql

---
+++ makbox/sql/tales/all_users_details.sql
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2016-2017 Barchampas Gerasimos <http://makbox.co.nf/>
+ * Makbox is a personal (staas) cloud.
+ *
+ * Makbox is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * Makbox is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+CREATE TABLE IF NOT EXISTS `all_users_details` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `instant` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `ip_address` varchar(32) NOT NULL,
+  `mac_address` varchar(32) NOT NULL,
+  `folder` varchar(64) NOT NULL,
+  `cookies` varchar(128) NOT NULL,
+  `name` varchar(32) NOT NULL,
+  `visit_profile` varchar(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
